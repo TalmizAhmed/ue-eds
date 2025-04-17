0be6ae321c9e0140448dc008bc37988d33adea2d
Talmiz Ahmed <talmiza@adobe.com>
2025-04-17 10:02:41 +0530

    Clean up

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -69,26 +69,13 @@ function handleAccordionNavigationInEditor(accordionEl, navigateTo) {
   handleAccordionNavigation(accordionEl, navigateTo, true);
   activeAccordionPanel = navigateTo.dataset.id;
 }
-function generateFragmentRendition(fragmentFieldWrapper) {
-  fragmentFieldWrapper.classList.add('fragment-overlay');
-}
 
 function annotateFormFragment(fragmentFieldWrapper, fragmentDefinition) {
-  fragmentFieldWrapper.classList.toggle('fragment-wrapper', true);
-  if (!fragmentFieldWrapper.classList.contains('edit-mode')) {
-    const newFieldWrapper = fragmentFieldWrapper.cloneNode(true);
-    newFieldWrapper.setAttribute('data-aue-type', 'component');
-    newFieldWrapper.setAttribute('data-aue-resource', `urn:aemconnection:${fragmentDefinition.properties['fd:path']}`);
-    newFieldWrapper.setAttribute('data-aue-model', 'form-fragment');
-    newFieldWrapper.setAttribute('data-aue-label', fragmentDefinition.label?.value || fragmentDefinition.name);
-    newFieldWrapper.classList.add('edit-mode');
-    // newFieldWrapper.replaceChildren();
-    fragmentFieldWrapper.insertAdjacentElement('afterend', newFieldWrapper);
-    generateFragmentRendition(newFieldWrapper, fragmentDefinition);
-  } else {
-    // fragmentFieldWrapper.replaceChildren();
-    generateFragmentRendition(fragmentFieldWrapper);
-  }
+  fragmentFieldWrapper.classList.add('fragment-wrapper', 'edit-mode');
+  fragmentFieldWrapper.setAttribute('data-aue-type', 'component');
+  fragmentFieldWrapper.setAttribute('data-aue-resource', `urn:aemconnection:${fragmentDefinition.properties['fd:path']}`);
+  fragmentFieldWrapper.setAttribute('data-aue-model', 'form-fragment');
+  fragmentFieldWrapper.setAttribute('data-aue-label', fragmentDefinition.label?.value || fragmentDefinition.name);
 }
 
 function getPropertyModel(fd) {
